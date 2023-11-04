556d6a1c4c8b023a34db93a13cf40b5febf1cf5c
Felipe Lima <felipelimars@outlook.com.br>
2023-11-04 17:47:31 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import Logo from "../../assets/Cox-Automotive.webp";
+
+const Header = () => {
+  return (
+    <div>
+      <div>index</div>
+      <img src={Logo} alt="Logotipo Cox Automotive"/>
+    </div>
+  );
+}
+
+export default Header;
